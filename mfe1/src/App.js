450733856c8dc7bf43a06acba18a482e3329bb84
ext-aitakyn <ext-aitakyn@bi.group>
2021-09-03 18:56:36 +0600

    Module federation

---
+++ mfe1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './Button';
+function App() {
+  return (
+    <div>
+      <h1>IBEC 1</h1>
+      <Button>
+        {' '}
+        IBEC 1 Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ mfe1/src/Button.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const Button = () => (
+  <button>Я из первого приложения</button>
+);
+export default Button;
